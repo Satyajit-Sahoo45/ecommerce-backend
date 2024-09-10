b35eb57881fd56e0f41ba773fbe80cce2e516409
Satyajit-Sahoo45 <sahoosatyajit2801@gmail.com>
2024-09-11 00:54:22 +0530

    remove tracked file

--- dist/router/cart.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const cart_controller_1 = require("../controller/cart.controller");
-const cartRouter = (0, express_1.Router)();
-cartRouter.post("/cart", cart_controller_1.createOrUpdateCart);
-cartRouter.get("/cart", cart_controller_1.getAllCarts);
-cartRouter.delete("/cart", cart_controller_1.deleteCart);
-exports.default = cartRouter;
